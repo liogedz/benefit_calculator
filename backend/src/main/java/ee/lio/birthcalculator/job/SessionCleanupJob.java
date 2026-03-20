c22205f7b070b832772ffde575835ed3831a0fcf
liogedz <aleksei.gedz@gmail.com>
2026-03-20 21:02:08 +0200

    added cleanup on startup-plus to scheduled

--- backend/src/main/java/ee/lio/birthcalculator/job/SessionCleanupJob.java
+++ backend/src/main/java/ee/lio/birthcalculator/job/SessionCleanupJob.java
@@ -3,6 +3,8 @@ package ee.lio.birthcalculator.job;
 import ee.lio.birthcalculator.repository.BenefitSessionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +18,12 @@ public class SessionCleanupJob {
 
     public SessionCleanupJob(BenefitSessionRepository repository) {
         this.repository = repository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    @Transactional
+    public void runOnStartup() {
+        cleanupOldSessions();
     }
 
     @Scheduled(cron = "0 0 3 * * *")
